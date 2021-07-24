6cc7be0a417c6e842a4a0f533df2c9e026691756
Lynnsey Martin <lmartin@hubspot.com>
2021-07-24 13:36:53 -0400

    fix unit tests

--- src/model/calculator.test.js
+++ src/model/calculator.test.js
@@ -3,19 +3,17 @@ import * as ValueTypes from './ValueTypes';
 
 describe('/model/calculator', () => {
   test('source result is the source value', () => {
-    const result = calculate({ type: ValueTypes.SOURCE, values: [1] }, 0);
-    expect(result.length).toBe(1);
-    expect(result[0]).toBe(1);
+    const result = calculate({ type: ValueTypes.SOURCE, values: [1, 0, 1] }, 0);
+    expect(result).toEqual([1, 0, 1]);
   });
 
   test('clock result is the clock value passed in', () => {
     const result0 = calculate({ type: ValueTypes.CLOCK, values: [] }, 0);
-    expect(result0.length).toBe(1);
+    expect(result0).toEqual([0]);
     expect(result0[0]).toBe(0);
 
     const result1 = calculate({ type: ValueTypes.CLOCK, values: [] }, 1);
-    expect(result1.length).toBe(1);
-    expect(result1[0]).toBe(1);
+    expect(result1).toEqual([1]);
   });
 
   describe('not', () => {
@@ -23,12 +21,11 @@ describe('/model/calculator', () => {
       const result = calculate(
         {
           type: ValueTypes.NOT,
-          values: [{ type: ValueTypes.SOURCE, values: [1] }],
+          values: [{ type: ValueTypes.SOURCE, values: [1, 0, 0, 1] }],
         },
         0
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(0);
+      expect(result).toEqual([0, 1, 1, 0]);
     });
 
     test('opposite of clock', () => {
@@ -39,8 +36,7 @@ describe('/model/calculator', () => {
         },
         0
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(1);
+      expect(result).toEqual([1]);
     });
 
     test('opposite of gate input', () => {
@@ -59,38 +55,35 @@ describe('/model/calculator', () => {
         },
         1
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(0);
+      expect(result).toEqual([0]);
     });
   });
 
   describe('or', () => {
-    test("sources aren't changed by clock", () => {
-      const result1 = calculate(
+    test('sources are not changed by clock', () => {
+      const result110 = calculate(
         {
           type: ValueTypes.OR,
           values: [
-            { type: ValueTypes.SOURCE, values: [0] },
-            { type: ValueTypes.SOURCE, values: [1] },
+            { type: ValueTypes.SOURCE, values: [0, 1, 0] },
+            { type: ValueTypes.SOURCE, values: [1, 1, 0] },
           ],
         },
         1
       );
-      expect(result1.length).toBe(1);
-      expect(result1[0]).toBe(1);
+      expect(result110).toEqual([1, 1, 0]);
 
-      const result0 = calculate(
+      const result11 = calculate(
         {
           type: ValueTypes.OR,
           values: [
-            { type: ValueTypes.SOURCE, values: [0] },
-            { type: ValueTypes.SOURCE, values: [1] },
+            { type: ValueTypes.SOURCE, values: [0, 0] },
+            { type: ValueTypes.SOURCE, values: [1, 1] },
           ],
         },
         0
       );
-      expect(result0.length).toBe(1);
-      expect(result0[0]).toBe(1);
+      expect(result11).toEqual([1, 1]);
     });
   });
 
@@ -106,8 +99,7 @@ describe('/model/calculator', () => {
         },
         1
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(0);
+      expect(result).toEqual([0]);
     });
 
     test('both off is off', () => {
@@ -121,8 +113,7 @@ describe('/model/calculator', () => {
         },
         1
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(0);
+      expect(result).toEqual([0]);
     });
 
     test('one on is on', () => {
@@ -136,8 +127,7 @@ describe('/model/calculator', () => {
         },
         1
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(1);
+      expect(result).toEqual([1]);
     });
   });
 
@@ -153,8 +143,7 @@ describe('/model/calculator', () => {
         },
         1
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(1);
+      expect(result).toEqual([1]);
     });
 
     test('with clock off', () => {
@@ -168,8 +157,7 @@ describe('/model/calculator', () => {
         },
         0
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(0);
+      expect(result).toEqual([0]);
     });
 
     test('complicated inputs', () => {
@@ -192,8 +180,7 @@ describe('/model/calculator', () => {
         },
         1
       );
-      expect(result.length).toBe(1);
-      expect(result[0]).toBe(1);
+      expect(result).toEqual([1]);
     });
   });
 });
